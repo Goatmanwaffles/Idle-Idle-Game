2e1bc467f4282aa8d77034b2d31581cdefd77e28
Goatmanwaffles <70929003+Goatmanwaffles@users.noreply.github.com>
2020-09-08 17:00:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var gameData = {
     money: 0,
-    moneyPerClick: 1,
     skillUpgradeCost: 100,
     skillUpgrades: 0,
     devs: 0,
     devCost: 1000,
+    devMulitplier: 1,
+    gameProgress:0,
+    gameProgressPerClick:1,
   }
 
   function makeGame() {
-      gameData.money += gameData.moneyPerClick
+      gameData.gameProgress += gameData.gameProgressPerClick
       document.getElementById("money").innerHTML = gameData.money + "$"
+      document.getElementById("gameProgress").innerHTML = "Game Progress" + gameData.gameProgress + "/10"
   }
-
-  function devProduce() {
-    gameData.money += gameData.devs
+  function makeMoney() {
+    gameData.money += Math.round(Math.floor(Math.random(1,5) * 6) + 1)
   }
 
   function buySkillUpgrade() {
@@ -33,9 +35,9 @@ if (gameData.money >= gameData.devCost) {
     gameData.devs += 1
     gameData.devCost = Math.round(Math.pow(gameData.devCost,1.01))
     document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
+} 
 }
 
- }
 
  var mainGameLoop = window.setInterval(function() {
     devProduce()
@@ -43,3 +45,14 @@ if (gameData.money >= gameData.devCost) {
     document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
     document.getElementById("skillUpgrade").innerHTML = "Upgrade Skill (Currently Level " + gameData.skillUpgrades + ") Cost: " + gameData.skillUpgradeCost + "$"
 }, 1000)
+
+var updateLoop = window.setInterval(function() {
+  if (gameData.gameProgress === 10) {
+    gameData.gameProgress = 0
+    makeMoney()
+  }
+  document.getElementById("money").innerHTML = gameData.money + "$"
+  document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
+  document.getElementById("skillUpgrade").innerHTML = "Upgrade Skill (Currently Level " + gameData.skillUpgrades + ") Cost: " + gameData.skillUpgradeCost + "$"
+  document.getElementById("gameProgress").innerHTML = "Game Progress:" + gameData.gameProgress + "/10"
+}, 10)
