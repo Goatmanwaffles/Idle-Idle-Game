9ae61336597d3398d699fa613b14dc01be69f1fb
Goatmanwaffles <70929003+Goatmanwaffles@users.noreply.github.com>
2020-09-09 21:05:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,21 @@ var gameData = {
     devMulitplier: 1,
     gameProgress:0,
     gameProgressPerClick:1,
+    ideas:0,
+    ideasPerClick:1,
   }
+  var testBody = document.getElementById("body")
+  var button = document.createElement("button")
 
-  function makeGame() {
-      gameData.gameProgress += gameData.gameProgressPerClick
-      document.getElementById("money").innerHTML = gameData.money + "$"
-      document.getElementById("gameProgress").innerHTML = "Game Progress" + gameData.gameProgress + "/10"
-  }
+
+function makeIdea() {
+  gameData.ideas += gameData.ideasPerClick
+}
+  //function makeGame() {
+    //  gameData.gameProgress += gameData.gameProgressPerClick
+      //document.getElementById("money").innerHTML = gameData.money + "$"
+      //document.getElementById("gameProgress").innerHTML = "Game Progress" + gameData.gameProgress + "/10"
+  //}
   function makeMoney() {
     gameData.money += Math.round(Math.floor(Math.random(1,5) * 6) + 1)
   }
@@ -39,20 +47,26 @@ if (gameData.money >= gameData.devCost) {
 }
 
 
- var mainGameLoop = window.setInterval(function() {
-    devProduce()
-    document.getElementById("money").innerHTML = gameData.money + "$"
-    document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
-    document.getElementById("skillUpgrade").innerHTML = "Upgrade Skill (Currently Level " + gameData.skillUpgrades + ") Cost: " + gameData.skillUpgradeCost + "$"
-}, 1000)
+ //var mainGameLoop = window.setInterval(function() {
+   //devProduce()
+   // document.getElementById("money").innerHTML = gameData.money + "$"
+   // document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
+   //  document.getElementById("skillUpgrade").innerHTML = "Upgrade Skill (Currently Level " + gameData.skillUpgrades + ") Cost: " + gameData.skillUpgradeCost + "$"
+//}, 1000)
 
 var updateLoop = window.setInterval(function() {
   if (gameData.gameProgress === 10) {
     gameData.gameProgress = 0
     makeMoney()
   }
+  if (gameData.ideas === 10) {
+    gameData.ideas = 0;
+  testBody.appendChild(button);
+  button.addEventListener ("click", makeMoney())
+  document.getElementById("body").appendChild(body)
+}
   document.getElementById("money").innerHTML = gameData.money + "$"
   document.getElementById("devPurchase").innerHTML = "Buy Game Dev (Currently:" + gameData.devs + ") Cost:" + gameData.devCost + "$"
   document.getElementById("skillUpgrade").innerHTML = "Upgrade Skill (Currently Level " + gameData.skillUpgrades + ") Cost: " + gameData.skillUpgradeCost + "$"
-  document.getElementById("gameProgress").innerHTML = "Game Progress:" + gameData.gameProgress + "/10"
+  document.getElementById("ideaProgress").innerHTML = "Game Progress:" + gameData.ideas + "/10"
 }, 10)
